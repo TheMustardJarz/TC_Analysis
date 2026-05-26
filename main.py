276df8b0d610ec1e981d879b4cda31993ee38542
Maxwell Collins <cmax647@gmail.com>
2026-05-25 20:30:10 -0400

    Template HTML

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from tc-analysis!")
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     main()
